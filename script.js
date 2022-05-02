0658cb11d3647d55947046c60557ffc3a7814217
Audrey Tabler <101310073+audreytabler@users.noreply.github.com>
2022-05-02 13:47:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-let block = document.getElementById("block");
-let hole = document.getElementById("hole");
-let character = document.getElemendById("character");
-let jumping = 0;
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
 
-
-HTMLBodyElement.addEventListener('animationiteration',() =>{
-    let random = (Math.random()*300)
+hole.addEventListener('animationiteration', () => {
+    var random = -((Math.random()*300)+150);
+    hole.style.top = random + "px";
+    counter++;
 });
-
-setInterval(function(){ //gravity
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-     //adds three pixels so every 10 ms it moves the ball down if not jumping
-    if (jumping ==0){
-        character.style.top=(characterTop+3)+"px";
+setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    //adds three pixels so every 10 ms it moves the ball down if not jumping
+    if(jumping==0){
+        character.style.top = (characterTop+3)+"px";
     }
-},10); //this function runs every 10 ms and will keep returning the top position of the character div
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500-characterTop);
+    if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
+        //alert("Game over. Score: "+(counter-1));
+        character.style.top = 100 + "px";
+        counter=0;
+    }
+},10);  //this function runs every 10 ms and will keep returning the top position of the character div
 
 function jump(){
-    jumping = 1
-    let jumpCount =0;
-    let jumpingInterval = setInterval(function(){
-        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue)
-        if((character >6) && (jumpCount<15)){
-            character.style.top = (characterTop -5)+"px"; //goes up 5 pixels
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px"; //go up 5 pixels
         }
-        //let characterTop = character.style.top=(characterTop-5)+"px"; //go up 5 pixels
-        if (jumpCount>15){
-            clearInterval(jumpingInterval)
-            jumping = 0;
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
             jumpCount=0;
         }
         jumpCount++;
-
     },10);
 }
